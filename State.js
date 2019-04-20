502acd38906d110fbfef568852fa9bcf3af17ed2
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 20:48:41 +0530

    adding State class

---
+++ State.js
@@ -0,0 +1,25 @@
+const speed = 0.05
+class State {
+
+    constructor() {
+        this.scale = 0
+        this.dir = 0
+        this.prevScale = 0
+    }
+
+    update(cb) {
+        this.scale += speed
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            cb()
+        }
+    }
+
+    startUpdating(cb) {
+        if (this.dir == 0 && this.scale == 0) {
+            this.dir = 1
+        }
+    }
+}
