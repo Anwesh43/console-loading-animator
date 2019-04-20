b2a6adf1f50634fac7bfc5437606c9fdad7a2aa0
Anwesh43 <anweshthecool0@gmail.com>
2019-04-20 20:45:36 +0530

    adding Animator class

---
+++ Animator.js
@@ -0,0 +1,16 @@
+const delay = 100
+class Animator {
+    start(cb) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
